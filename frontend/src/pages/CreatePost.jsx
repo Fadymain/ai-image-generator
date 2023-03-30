fbadb83d6bf36d517d853ea72054d3fafdf49aa8
Adetola <adetolafadairo@gmail.com>
2023-03-30 11:14:01 -0700

    implementing handleSurpriseMe function

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -27,8 +27,9 @@ const CreatePost = () => {
     setForm({ ...form, [e.target.name]: e.target.value })
   };
 
-  const handleSupriseMe = () => {
-
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt })
   };
 
   return (
@@ -56,7 +57,7 @@ const CreatePost = () => {
             value={form.prompt}
             handleChange={handleChange}
             isSurpriseMe
-            handleSupriseMe={handleSupriseMe}
+            handleSupriseMe={handleSurpriseMe}
           />
 
           <div className='relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center'>
