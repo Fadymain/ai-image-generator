ad0d71fb2506bbf39da1a11b0e9698c206944d0d
Adetola <adetolafadairo@gmail.com>
2023-06-06 11:36:25 -0700

    updating get route

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -16,7 +16,13 @@ cloudinary.config({
 
 // GET ALL ROUTES
 router.route('/').get(async(req, res) => {
+    try {
+        const posts = await Post.find({});
 
+        res.status(200).json({ success: true, data: posts })
+    } catch (error) {
+        res.status(500).json({ success: false, message: error })
+    }
 });
 
 // CREATE A POST
