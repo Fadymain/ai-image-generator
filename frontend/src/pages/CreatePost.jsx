ec0c8c91eacb48c190e67ef6479a0a7f5d3cd67b
Adetola <adetolafadairo@gmail.com>
2023-06-07 10:53:11 -0700

    moving try catch into right block

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -40,8 +40,30 @@ const CreatePost = () => {
     }
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = async (e) => {
+    e.prevent.default();
 
+    if(form.prompt && form.prompt) {
+      setLoading(true);
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post',{
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(form)
+        })
+  
+        await response.json();
+        navigate('/');
+      } catch (error) {
+        alert(error);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert('Please enter a prompt and generate an image')
+    }
   };
 
   const handleChange = (e) => {
