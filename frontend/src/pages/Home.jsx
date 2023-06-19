7c2d702b7a06c24a8c431564757dceeadb582452
Adetola <adetolafadairo@gmail.com>
2023-06-19 11:38:52 -0700

    updating classname for rendercards

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,7 +7,7 @@ const RenderCards = ({data, title}) => {
   }
 
   return (
-    <h2 className='mt-5 font-bold text-[#6449ff]'>{title}</h2>
+    <h2 className='mt-5 font-bold text-[#6449ff] text-xl uppercase'>{title}</h2>
   )
 }
 
