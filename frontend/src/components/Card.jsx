2e489a07e6d17ebacde1f494943182c0f720fa13
Adetola <adetolafadairo@gmail.com>
2023-07-06 11:31:08 -0700

    adding button and styling to component

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -11,7 +11,7 @@ const Card = ({ _id, name, prompt, photo }) => {
         alt={prompt}
       />
       <div className='group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 lleft-0 righttt-0 bg-[#10131f] m-2 p-4 rounded-md'>
-        <p className='text-white text-sm overflow-y-auto prompt'>
+        <p className='text-white text-md overflow-y-auto prompt'>
           {prompt}
         </p>
 
@@ -20,7 +20,11 @@ const Card = ({ _id, name, prompt, photo }) => {
             <div className='w-7 h-7 rounded-full object-cover bg-green-700 flex justify-center items-center text-white text-xs font-bold'>
               {/* {name[0]} */}
             </div>
+            <p className='text-white text-sm'>{name}</p>
           </div>
+          <button type='button' onClick={() => downloadImage(_id, photo)} className='outline-none bg-transparent border-none'>
+            <img src={download} alt='download' className='w-6 h-6 object-contain invert'/>
+          </button>
         </div>
       </div>
     </div>
