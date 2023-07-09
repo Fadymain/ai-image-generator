e943eb98ff1d89c6b76b3a451b909339eb20e109
Adetola <adetolafadairo@gmail.com>
2023-07-09 14:34:34 -0700

    adding limit to bodyParser

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '50mb' }));
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes);
